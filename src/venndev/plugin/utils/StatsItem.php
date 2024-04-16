30caa9b5e0573e2359a50f69e91b06193020e03e
Di4r <119154247+DiarDev@users.noreply.github.com>
2024-04-16 11:31:09 +0700

    Fix data transfer error in constant variable

--- src/venndev/plugin/utils/StatsItem.php
+++ src/venndev/plugin/utils/StatsItem.php
@@ -7,32 +7,32 @@ namespace venndev\plugin\utils;
 trait StatsItem
 {
 
-    public const string HEAD_TAG = "v_item";
-    public const string DAMAGE = "damage";
-    public const string DEFENSE = "defense";
-    public const string TRUE_DEFENSE = "true_defense";
-    public const string HEALTH = "health";
-    public const string SPEED = "speed";
-    public const string CRITICAL_CHANCE = "critical_chance";
-    public const string CRITICAL_DAMAGE = "critical_damage";
-    public const string STRENGTH = "strength";
-    public const string INTELLIGENCE = "intelligence";
-    public const string FISHING_SPEED = "fishing_speed";
-    public const string FISHING_LUCK = "fishing_luck";
-    public const string SEA_CREATURE_CHANCE = "sea_creature_chance";
-    public const string MAGIC_FIND = "magic_find";
-    public const string PET_LUCK = "pet_luck";
-    public const string BONUS_ATTACK_SPEED = "bonus_attack_speed";
-    public const string MINING_SPEED = "mining_speed";
-    public const string MINING_FORTUNE = "mining_fortune";
-    public const string FARMING_FORTUNE = "farming_fortune";
-    public const string FORAGING_FORTUNE = "foraging_fortune";
-    public const string ABILITY_DAMAGE = "ability_damage";
-    public const string ARROW_DAMAGE = "arrow_damage";
-    public const string ARROW_PIERCING = "arrow_piercing";
+    public const HEAD_TAG = "v_item";
+    public const DAMAGE = "damage";
+    public const DEFENSE = "defense";
+    public const TRUE_DEFENSE = "true_defense";
+    public const HEALTH = "health";
+    public const SPEED = "speed";
+    public const CRITICAL_CHANCE = "critical_chance";
+    public const CRITICAL_DAMAGE = "critical_damage";
+    public const STRENGTH = "strength";
+    public const INTELLIGENCE = "intelligence";
+    public const FISHING_SPEED = "fishing_speed";
+    public const FISHING_LUCK = "fishing_luck";
+    public const SEA_CREATURE_CHANCE = "sea_creature_chance";
+    public const MAGIC_FIND = "magic_find";
+    public const PET_LUCK = "pet_luck";
+    public const BONUS_ATTACK_SPEED = "bonus_attack_speed";
+    public const MINING_SPEED = "mining_speed";
+    public const MINING_FORTUNE = "mining_fortune";
+    public const FARMING_FORTUNE = "farming_fortune";
+    public const FORAGING_FORTUNE = "foraging_fortune";
+    public const ABILITY_DAMAGE = "ability_damage";
+    public const ARROW_DAMAGE = "arrow_damage";
+    public const ARROW_PIERCING = "arrow_piercing";
 
     // Maybe this is for physical damage?
-    public const string FEROCITY = "ferocity";
+    public const FEROCITY = "ferocity";
 
     public function removeHeadTagFromString(string $tag) : string
     {
